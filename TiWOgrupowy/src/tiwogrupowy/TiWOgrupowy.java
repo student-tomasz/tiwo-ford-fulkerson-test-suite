c3c83dec7ff6d48fbf0e36ea415abc7ea5d61645
Ochteram <spaczn@gmail.com>
2012-12-05 17:05:23 +0100

    Załóż pusty projekt w NetBeans

---
+++ TiWOgrupowy/src/tiwogrupowy/TiWOgrupowy.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiwogrupowy;
+
+/**
+ *
+ * @author demon
+ */
+public class TiWOgrupowy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
