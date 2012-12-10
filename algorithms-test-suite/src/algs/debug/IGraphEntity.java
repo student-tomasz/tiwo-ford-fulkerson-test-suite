72e9f983665ce2d3824fadfa829866300adeefb7
Tomasz Cudziło <tomasz@luzid.co>
2012-12-10 01:08:16 +0100

    Popraw kodowanie znaków, by móc skompilować projekt

--- algorithms-test-suite/src/algs/debug/IGraphEntity.java
+++ algorithms-test-suite/src/algs/debug/IGraphEntity.java
@@ -5,11 +5,11 @@ import algs.model.gametree.MoveEvaluation;
 /**
  * All graphical state drawing depends on the individual nodes to be able to return a labeled
  * string to represent itself.
- * 
+ *
  * We distribute the logic to the individual entities themselves, which simplifies
  * the drawing of debugging information at the expense of adding this extra method
  * to classes which otherwise should be unaware of the debugging infrastructure.
- * 
+ *
  * @author George Heineman
  * @version 1.0, 6/15/08
  * @since 1.0
@@ -21,7 +21,7 @@ public interface IGraphEntity {
 
 	/** Useful Format for special characters. */
 	public static class Formatter {
-		
+
 		/**
 		 * Helper method to determine if special font is needed.
 		 */
@@ -32,22 +32,22 @@ public interface IGraphEntity {
 				return true;
 			}
 
-			return false;  // nothing special.			
+			return false;  // nothing special.
 		}
-		
+
 		/**
 		 * Helper method for converting values into properly visible labels.
-		 * 
-		 * These special characters are displayed in Symbol font. 
+		 *
+		 * These special characters are displayed in Symbol font.
 		 */
 		public static final String convert (int value) {
 			if (value == MoveEvaluation.minimum()) {
-				return "-¥";  // - INF using an em-dash
+				return "-âˆž";  // - INF using an em-dash
 			} else if (value == MoveEvaluation.maximum()) {
-				return "¥";   // that is char for INF in Symbol font.
+				return "âˆž";   // that is char for INF in Symbol font.
 			}
 
 			return "" + value;
-		}	
+		}
 	}
 }
