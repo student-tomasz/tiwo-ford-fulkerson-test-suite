50f5666d9953bb1299a70ea6829829a7cdf59a75
Ochteram <spaczn@gmail.com>
2012-12-12 02:11:48 +0100

    Wprowadź poprawki w Main.java

--- ford-fulkerson-test-suite/src/algs/network/Main.java
+++ ford-fulkerson-test-suite/src/algs/network/Main.java
@@ -13,11 +13,12 @@ import java.util.Iterator;
  */
 public class Main {
     
+    
     public static final void main(String[] args){
         
         int numVertices = 6; //ilość węzłów w grafie
         int srcIndex = 0; //indeks węzła źródłowego
-        int sinkIndex = 5; //indeks węzła docelowego
+        int sinkIndex = 5;  //indeks węzła docelowego
         
         //Tworzę poszczególne krawędzie i przypisuję im przpustowość
         ArrayList preIterator = new ArrayList();
@@ -37,19 +38,36 @@ public class Main {
         preIterator.add(edge6);
         preIterator.add(edge7);
         preIterator.add(edge8);
-                
-                
-        Iterator<EdgeInfo> edges = preIterator.iterator(); //zbiór krawędzi grafu
+                                
+        Iterator<EdgeInfo> edges = preIterator.iterator();  //zbiór krawędzi grafu
+        Iterator<EdgeInfo> edges2 = preIterator.iterator();  //zbiór krawędzi grafu
         
-        FlowNetworkArray network = new FlowNetworkArray(numVertices, srcIndex, sinkIndex, edges);  //graf przepływu sieci
+        
+        
+
+        System.out.println("Wersja z podwójną listą. Pamięciożerna");
+        FlowNetworkAdjacencyList network = new FlowNetworkAdjacencyList(numVertices, srcIndex, sinkIndex, edges);  //graf przepływu sieci na listach
         
         System.out.println("Przed:");
         System.out.println(network.toString());
-        BFS_SearchArray search = new BFS_SearchArray(network);//algorytm przeszukiwania wszerz
+        BFS_SearchList search = new BFS_SearchList(network);//algorytm przeszukiwania wszerz
         FordFulkerson fordFulkerson = new FordFulkerson(network, search);
         fordFulkerson.compute();//wykonaj algorytm
         
         System.out.println("Po:");
         System.out.println(network.toString());
-    }
+        
+        
+        System.out.println("Wersja z macierzą dwuwymiarową. Oszczedniejsza");   
+        FlowNetworkArray networkArray = new FlowNetworkArray(numVertices, srcIndex, sinkIndex, edges2);  //graf przepływu sieci na macierzach
+        
+        System.out.println("Przed:");
+        System.out.println(networkArray.toString());
+        BFS_SearchArray searchArray = new BFS_SearchArray(networkArray);//algorytm przeszukiwania wszerz
+        fordFulkerson = new FordFulkerson(networkArray, searchArray);
+        fordFulkerson.compute();//wykonaj algorytm
+        
+        System.out.println("Po:");
+        System.out.println(networkArray.toString());
+    }   
 }
