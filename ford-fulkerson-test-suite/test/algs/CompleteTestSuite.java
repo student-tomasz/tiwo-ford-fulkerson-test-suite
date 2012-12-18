2ea53d78fae91c067a94e43a17cdd817ab64c346
Tomasz Cudziło <tomasz@luzid.co>
2012-12-18 23:45:55 +0100

    Dodaj zbiorcze TestSuites do szczegółowego projektu

---
+++ ford-fulkerson-test-suite/test/algs/CompleteTestSuite.java
@@ -0,0 +1,23 @@
+package algs;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    algs.model.performance.network.DegenerateCaseTest.class,
+    algs.model.performance.network.Figure8_3Test.class,
+    algs.model.performance.network.Figure8_7Test.class,
+    algs.model.performance.network.Figure_FactSheet_FordFulkersonTest.class,
+    algs.model.performance.network.FinalCaseTest.class,
+    algs.network.CormenExampleTest.class,
+    algs.network.CreateImageTest.class,
+    algs.network.ExampleTest.class,
+    algs.network.MaxFlowMinCostTest.class,
+    algs.network.SampleBackflowTest.class,
+    algs.network.SimpleTest.class,
+    algs.network.WebExampleTest.class
+})
+public class CompleteTestSuite {
+    
+}
---
+++ ford-fulkerson-test-suite/test/algs/CompleteWithoutPerformanceTestSuite.java
@@ -0,0 +1,18 @@
+package algs;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    algs.network.CormenExampleTest.class,
+    algs.network.CreateImageTest.class,
+    algs.network.ExampleTest.class,
+    algs.network.MaxFlowMinCostTest.class,
+    algs.network.SampleBackflowTest.class,
+    algs.network.SimpleTest.class,
+    algs.network.WebExampleTest.class
+})
+public class CompleteWithoutPerformanceTestSuite {
+    
+}
