7fa3e1ca26feca2476cafee45f423faff1fc5c1a
Robert Wróblewski <wroblewrobert@gmail.com>
2013-01-07 21:10:43 +0100

    Dodaj test na sieci z ujemną liczbą wierzchołków (nie da się takiej stworzyć).

---
+++ test-suite/test/rw/blackbox/NegativeSizeNetworkTest.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rw.blackbox;
+
+import algs.network.DFS_SearchArray;
+import algs.network.EdgeInfo;
+import algs.network.FlowNetwork;
+import algs.network.FlowNetworkArray;
+import algs.network.FordFulkerson;
+import algs.network.generator.FlowNetworkGenerator;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+/**
+ *
+ * @author robert
+ */
+public class NegativeSizeNetworkTest {
+    FordFulkerson fulkerson;
+    public NegativeSizeNetworkTest() {
+    }
+    
+    @Test(expectedExceptions=NegativeArraySizeException.class)
+    public void FailAtNegativeSizeNetworkTest(){
+        FlowNetwork<EdgeInfo[][]> siecPrzeplywu = new FlowNetworkArray(-5, 2, 5, null);
+        fulkerson = new FordFulkerson((siecPrzeplywu), new DFS_SearchArray(siecPrzeplywu));
+    }
+
+    @Test(expectedExceptions=NegativeArraySizeException.class)
+    public void FailAtNegativeSizeNetworkTest2() throws Exception {
+        FlowNetwork<EdgeInfo[][]> siecPrzeplywu = FlowNetworkGenerator.generateArray(-5, 1, 5, 3, 10);
+        fulkerson = new FordFulkerson((siecPrzeplywu), new DFS_SearchArray(siecPrzeplywu));
+    }
+    
+    //Nie da się stworzyć sieci z ujemną wartością numVertices
+}
