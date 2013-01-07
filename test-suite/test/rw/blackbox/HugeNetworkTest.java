9e6edf789cee063381fdf459bf34e529c7318101
Robert Wróblewski <wroblewrobert@gmail.com>
2013-01-07 22:57:36 +0100

    Popraw testy dla dużej ilości węzłów w klasie HugeNetworkTest

--- test-suite/test/rw/blackbox/HugeNetworkTest.java
+++ test-suite/test/rw/blackbox/HugeNetworkTest.java
@@ -11,6 +11,7 @@ import algs.network.FordFulkerson;
 import algs.network.generator.FlowNetworkGenerator;
 import static org.testng.Assert.*;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 /**
@@ -22,15 +23,90 @@ public class HugeNetworkTest {
     public HugeNetworkTest() {
     }
 
+    @BeforeSuite
+    public void SetGeneratorSeed(){
+        FlowNetworkGenerator.setSeed(System.nanoTime()+System.currentTimeMillis());
+    }
+    
     @Test
-    public void failAtCompute(){
+    public void Vertices1e1Test(){
+        System.gc();
+        FlowNetwork<EdgeInfo[][]> siecPrzeplywu = FlowNetworkGenerator.generateArray(10, 1, 5, 1, 4);
+        fulkerson = new FordFulkerson((siecPrzeplywu), new DFS_SearchArray(siecPrzeplywu));
+        assertEquals(fulkerson.compute(), true, "Nie znaleziono ścieżki powiększającej.");
+        assertEquals(fulkerson.compute(),false,"Znaleziono ścieżkę powiększającej w sprawdzonej sieci przepływu.");
+    }
+    
+    @Test
+    public void Vertices1e2Test(){
+        System.gc();
+        FlowNetwork<EdgeInfo[][]> siecPrzeplywu = FlowNetworkGenerator.generateArray(100, 1, 5, 1, 4);
+        fulkerson = new FordFulkerson((siecPrzeplywu), new DFS_SearchArray(siecPrzeplywu));
+        assertEquals(fulkerson.compute(), true, "Nie znaleziono ścieżki powiększającej.");
+        assertEquals(fulkerson.compute(),false,"Znaleziono ścieżkę powiększającej w sprawdzonej sieci przepływu.");
+    }
+    
+    @Test
+    public void Vertices1e3Test(){
+        System.gc();
+        FlowNetwork<EdgeInfo[][]> siecPrzeplywu = FlowNetworkGenerator.generateArray(1000, 1, 5, 1, 4);
+        fulkerson = new FordFulkerson((siecPrzeplywu), new DFS_SearchArray(siecPrzeplywu));
+        assertEquals(fulkerson.compute(), true, "Nie znaleziono ścieżki powiększającej.");
+        assertEquals(fulkerson.compute(),false,"Znaleziono ścieżkę powiększającej w sprawdzonej sieci przepływu.");
+    }
+       
+    /*@Test
+    public void Vertices1e4Test(){
+        System.gc();
+        FlowNetwork<EdgeInfo[][]> siecPrzeplywu = FlowNetworkGenerator.generateArray(10000, 4, 12, 2, 7);
+        fulkerson = new FordFulkerson((siecPrzeplywu), new DFS_SearchArray(siecPrzeplywu));
+        assertEquals(fulkerson.compute(), true, "Nie znaleziono ścieżki powiększającej.");
+        assertEquals(fulkerson.compute(),false,"Znaleziono ścieżkę powiększającej w sprawdzonej sieci przepływu.");
+    }
+    
+    @Test
+    public void Vertices1e5Test(){
+        System.gc();
+        FlowNetwork<EdgeInfo[][]> siecPrzeplywu = FlowNetworkGenerator.generateArray(100000, 2, 7, 4, 6);
+        fulkerson = new FordFulkerson((siecPrzeplywu), new DFS_SearchArray(siecPrzeplywu));
+        assertEquals(fulkerson.compute(), true, "Nie znaleziono ścieżki powiększającej.");
+        assertEquals(fulkerson.compute(),false,"Znaleziono ścieżkę powiększającej w sprawdzonej sieci przepływu.");
+    }
+        
+    @Test
+    public void Vertices1e6Test(){
+        System.gc();
+        FlowNetwork<EdgeInfo[][]> siecPrzeplywu = FlowNetworkGenerator.generateArray(1000000, 1, 10, 5, 9);
+        fulkerson = new FordFulkerson((siecPrzeplywu), new DFS_SearchArray(siecPrzeplywu));
+        assertEquals(fulkerson.compute(), true, "Nie znaleziono ścieżki powiększającej.");
+        assertEquals(fulkerson.compute(),false,"Znaleziono ścieżkę powiększającej w sprawdzonej sieci przepływu.");
+    }
+    
+    @Test
+    public void Vertices1e7Test(){
+        System.gc();
+        FlowNetwork<EdgeInfo[][]> siecPrzeplywu = FlowNetworkGenerator.generateArray(10000000, 1, 5, 3, 4);
+        //fulkerson = new FordFulkerson((siecPrzeplywu), new DFS_SearchArray(siecPrzeplywu));
+        //assertEquals(fulkerson.compute(), true, "Nie znaleziono ścieżki powiększającej.");
+        //assertEquals(fulkerson.compute(),false,"Znaleziono ścieżkę powiększającej w sprawdzonej sieci przepływu.");
+    }
+    
+    @Test
+    public void Vertices1e8Test(){
+        System.gc();
+        FlowNetwork<EdgeInfo[][]> siecPrzeplywu = FlowNetworkGenerator.generateArray(100000000, 1, 4, 1, 3);
+        fulkerson = new FordFulkerson((siecPrzeplywu), new DFS_SearchArray(siecPrzeplywu));
         assertEquals(fulkerson.compute(), true, "Nie znaleziono ścieżki powiększającej.");
         assertEquals(fulkerson.compute(),false,"Znaleziono ścieżkę powiększającej w sprawdzonej sieci przepływu.");
     }
 
-    @BeforeMethod
-    public void setUpMethod() throws Exception {
-        FlowNetwork<EdgeInfo[][]> siecPrzeplywu = FlowNetworkGenerator.generateArray(1000000000, 1, 5, 1, 4);
+    @Test
+    public void Vertices1e9Test(){
+        System.gc();
+        FlowNetwork<EdgeInfo[][]> siecPrzeplywu = FlowNetworkGenerator.generateArray(1000000000, 2, 5, 3, 6);
         fulkerson = new FordFulkerson((siecPrzeplywu), new DFS_SearchArray(siecPrzeplywu));
-    }
+        assertEquals(fulkerson.compute(), true, "Nie znaleziono ścieżki powiększającej.");
+        assertEquals(fulkerson.compute(),false,"Znaleziono ścieżkę powiększającej w sprawdzonej sieci przepływu.");
+    }*/
+
 }
