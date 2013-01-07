62714b9100c6428ed40910e7f4eb7af0e331ceaa
Robert Wróblewski <wroblewrobert@gmail.com>
2013-01-07 21:34:47 +0100

    Dodaj czwarty test na sieci z dwoma węzłami połączonymi równolegle.

--- test-suite/test/rw/blackbox/TwoParallelVerticesTest.java
+++ test-suite/test/rw/blackbox/TwoParallelVerticesTest.java
@@ -105,4 +105,31 @@ public class TwoParallelVerticesTest {
         assertFalse(fulkerson.compute(),"Algorytm znalazł nowy przepływ maksymalny w uprzednio sprawdzonej sieci.");
     }
     
+    @Test
+    public void testCase4() throws Exception {
+        preIterator = new ArrayList(); // krawedzie i ich przepustowosc
+        EdgeInfo edge1 = new EdgeInfo(0, 1, 7);
+        EdgeInfo edge2 = new EdgeInfo(0, 2, 1);
+        EdgeInfo edge3 = new EdgeInfo(1, 3, 5);
+        EdgeInfo edge4 = new EdgeInfo(2, 3, 3);
+        EdgeInfo edge5 = new EdgeInfo(2, 1, 9);
+        EdgeInfo edge6 = new EdgeInfo(1, 2, 6);
+        preIterator.add(edge1);
+        preIterator.add(edge2);
+        preIterator.add(edge3);
+        preIterator.add(edge4);
+        preIterator.add(edge5);
+        preIterator.add(edge6);
+        
+        edges = preIterator.iterator();
+        
+        siecPrzeplywu=new FlowNetworkArray(4, 0, 3, edges);
+        
+        fulkerson=new FordFulkerson(siecPrzeplywu, new DFS_SearchArray(siecPrzeplywu));
+        
+        assertTrue(fulkerson.compute(),"Algorytm nie wyznaczył maksymalnego przepływu.");
+        
+        assertFalse(fulkerson.compute(),"Algorytm znalazł nowy przepływ maksymalny w uprzednio sprawdzonej sieci.");
+    }
+    
 }
