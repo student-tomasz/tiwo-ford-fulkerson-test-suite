c0a5b9875501de890b5de5c4c5a7a5a95f3307f0
Robert Wróblewski <wroblewrobert@gmail.com>
2013-01-08 16:57:04 +0100

    Dodaj test nr.7 na sieci z dwoma wierzchołkami pośrednimi połączonymi równolegle.

--- test-suite/test/rw/blackbox/TwoParallelVerticesTest.java
+++ test-suite/test/rw/blackbox/TwoParallelVerticesTest.java
@@ -190,4 +190,31 @@ public class TwoParallelVerticesTest {
         assertFalse(fulkerson.compute(),"Algorytm znalazł nowy przepływ maksymalny w uprzednio sprawdzonej sieci.");
     }
     
+    @Test
+    public void testCase7() throws Exception {
+        preIterator = new ArrayList(); // krawedzie i ich przepustowosc
+        EdgeInfo edge1 = new EdgeInfo(0, 1, 1);
+        EdgeInfo edge2 = new EdgeInfo(0, 2, 9);
+        EdgeInfo edge3 = new EdgeInfo(1, 3, 12);
+        EdgeInfo edge4 = new EdgeInfo(2, 3, 4);
+        EdgeInfo edge5 = new EdgeInfo(3, 1, 3);
+        preIterator.add(edge1);
+        preIterator.add(edge2);
+        preIterator.add(edge3);
+        preIterator.add(edge4);
+        preIterator.add(edge5);
+        
+        edges = preIterator.iterator();
+        
+        siecPrzeplywu=new FlowNetworkArray(4, 0, 3, edges);
+        
+        fulkerson=new FordFulkerson(siecPrzeplywu, new DFS_SearchArray(siecPrzeplywu));
+        
+        assertTrue(fulkerson.compute(),"Algorytm nie wyznaczył maksymalnego przepływu.");
+        
+        assertEquals(siecPrzeplywu, 4, "Wyznaczony przepływ jest inny niż określa wyrocznia.");
+        
+        assertFalse(fulkerson.compute(),"Algorytm znalazł nowy przepływ maksymalny w uprzednio sprawdzonej sieci.");
+    }
+    
 }
