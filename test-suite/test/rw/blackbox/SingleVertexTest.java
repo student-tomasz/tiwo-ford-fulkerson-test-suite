6f457dfedbebac4e393c08a7d2fd559cf57784b4
Robert Wróblewski <wroblewrobert@gmail.com>
2013-01-08 19:10:43 +0100

    Dodaj test nr 8 na sieci z jednym węzłem pośrednim, bez ścieżki pomiędzy źródłem, a ujściem.

--- test-suite/test/rw/blackbox/SingleVertexTest.java
+++ test-suite/test/rw/blackbox/SingleVertexTest.java
@@ -227,4 +227,25 @@ public class SingleVertexTest {
         
         assertFalse(fulkerson.compute(),"Algorytm znalazł nowy przepływ maksymalny w uprzednio sprawdzonej sieci.");
     }
+    
+    @Test
+    public void testCase8() throws Exception {
+        preIterator = new ArrayList(); // krawedzie i ich przepustowosc
+        EdgeInfo edge1 = new EdgeInfo(0, 1, 16);
+        EdgeInfo edge2 = new EdgeInfo(1, 2, 8);
+        //EdgeInfo edge3 = new EdgeInfo(1, 0, 5);
+        preIterator.add(edge1);
+        preIterator.add(edge2);
+        //preIterator.add(edge3);
+        
+        edges = preIterator.iterator();
+        
+        siecPrzeplywu=new FlowNetworkArray(4, 0, 2, edges);
+        
+        fulkerson=new FordFulkerson(siecPrzeplywu, new DFS_SearchArray(siecPrzeplywu));
+        
+        assertFalse(fulkerson.compute(),"Algorytm znalazł niezerowy przepływ maksymalny w sieci bez ścieżki ze źródła do ujścia.");
+        
+        assertEquals(siecPrzeplywu.getFlow(), 0, "Wyznaczony przepływ jest inny niż określa wyrocznia.");
+    }
 }
